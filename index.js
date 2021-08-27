93d727d84bfafdfc263488618407e5ab23caaf2f
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-08-27 19:15:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const path=require('path');
+
+console.log(path.basename("D:/Node Working/NodePath/index.js"));
+console.log(path.dirname("D:/Node Working/NodePath/index.js"));
+console.log(path.extname("D:/Node Working/NodePath/index.js"));
+// console.log(path.format("D:/Node Working/NodePath/index.js"));
+// console.log(path.win32("D:/Node Working/NodePath/index.js"));
